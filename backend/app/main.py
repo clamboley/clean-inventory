8d77a9c77b02aa62cf84f1f042ebdbf653944157
clamboley <corentin.lamboley@protonmail.com>
2025-09-20 19:11:12 +0200

    chore: add health check on main entrypoint

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI  # noqa: INP001
 
 from app.api.controllers import general_router
 from app.core.injector import lifespan
@@ -6,3 +6,18 @@ from app.core.injector import lifespan
 app = FastAPI(title="Inventory App", lifespan=lifespan)
 
 app.include_router(general_router)
+
+@app.get("/")
+async def root() -> dict:
+    """Root endpoint of the application."""
+    return {"message": "Gateway of the App"}
+
+
+@app.get(
+    "/health",
+    summary="Health check endpoint",
+    description="Endpoint to check the health of the application.",
+)
+def health_check() -> dict[str, str]:
+    """Health check endpoint."""
+    return {"status": "ok"}
